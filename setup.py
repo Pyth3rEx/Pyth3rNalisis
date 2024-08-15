40c0baaff2e4118e5c8de497ad8ce395525db611
Pyth3rEx <pyth3rex@outlook.com>
2024-08-15 14:24:10 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,11 @@ setup(
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Operating System :: OS Independent',
+        'Topic :: Security',
+        'Natural Language :: English',
+        'Intended Audience :: System Administrators',
+        'Intended Audience :: Information Technology',
+        'Development Status :: 3 - Alpha'
     ],
     python_requires='>=3.12',
 )
