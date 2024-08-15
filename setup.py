1470688bf4b1bd5fb5a388e53c1e867d95c7d259
Pyth3rEx <pyth3rex@outlook.com>
2024-08-15 13:08:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,39 @@
 from setuptools import setup, find_packages
-import os
-
-def parse_requirements(filename):
-    with open(filename, 'r') as file:
-        return [line for line in file.read().splitlines() if line and not line.startswith('#')]
-
-current_dir = os.path.abspath(os.path.dirname(__file__))
-readme_path = os.path.join(current_dir, 'README.md')
-
-with open(readme_path, 'r') as fh:
-    long_description = fh.read()
 
 setup(
     name='Pyth3rNalisis',
     version='0.0.1',
     description='Pyth3rNalisis is a malware analysis tool that searches for red flags in any file.',
-    long_description=long_description,
+    long_description='README.md',
     long_description_content_type='text/markdown',
     author='Pyth3rEx',
     author_email='',
     url='https://github.com/Pyth3rEx/Pyth3rNalisis',
     packages=find_packages(),
-    install_requires=parse_requirements(os.path.join(current_dir, 'requirements.txt')),
+    install_requires=["""
+        cffi==1.17.0
+        colorama==0.4.6
+        colorclass==2.2.2
+        cryptography==43.0.0
+        easygui==0.98.3
+        filetype==1.2.0
+        lxml==5.3.0
+        msoffcrypto-tool==5.4.2
+        olefile==0.47
+        oletools==0.60.2
+        pcodedmp==1.2.6
+        pefile==2023.2.7
+        pycparser==2.22
+        pyfiglet==1.0.2
+        pyparsing==3.1.2
+        PyPDF2==3.0.1
+        python-docx==1.1.2
+        python-magic==0.4.27
+        python-magic-bin==0.4.14
+        tabulate==0.9.0
+        typing_extensions==4.12.2
+        win_unicode_console==0.5
+    """],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
