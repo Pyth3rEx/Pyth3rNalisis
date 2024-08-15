b47249055fdbe10a44d8fd56ff4b58b4bf83dba3
Pyth3rEx <pyth3rex@outlook.com>
2024-08-15 13:25:53 +0200

    v0.0.2

--- src/Pyth3rNalisis.py
+++ src/Pyth3rNalisis.py
@@ -9,7 +9,7 @@ import modules.worker_metadata as worker_metadata
 import modules.worker_extension as worker_extension
 
 #constants
-version = '0.0.1'
+version = '0.0.2'
 
 # Custom ArgumentParser class
 class CustomArgumentParser(argparse.ArgumentParser):
