2495a0f4be17c88631aff0f6fe21ae20f327f6ed
Pyth3rEx <pyth3rex@outlook.com>
2024-08-22 13:38:08 +0200

    upgraded CLI

--- setup.py
+++ setup.py
@@ -9,36 +9,37 @@ setup(
     author='Pyth3rEx',
     author_email='',
     url='https://github.com/Pyth3rEx/Pyth3rNalisis',
-    packages=find_packages(),
-    install_requires=["""
-        certifi>=2024.7.4
-        cffi>=1.17.0
-        charset-normalizer>=3.3.2
-        colorama>=0.4.6
-        colorclass>=2.2.2
-        cryptography>=43.0.0
-        easygui>=0.98.3
-        filetype>=1.2.0
-        idna>=3.7
-        lxml>=5.3.0
-        msoffcrypto-tool>=5.4.2
-        olefile>=0.47
-        oletools>=0.60.2
-        pcodedmp>=1.2.6
-        pefile>=2023.2.7
-        pycparser>=2.22
-        pyfiglet>=1.0.2
-        pyparsing>=3.1.2
-        PyPDF2>=3.0.1
-        python-docx>=1.1.2
-        python-magic>=0.4.27
-        requests>=2.32.3
-        setuptools>=73.0.1
-        tabulate>=0.9.0
-        typing_extensions>=4.12.2
-        urllib3>=2.2.2
-        win_unicode_console>=0.5
-    """],
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    install_requires=[
+        "certifi>=2024.7.4",
+        "cffi>=1.17.0",
+        "charset-normalizer>=3.3.2",
+        "colorama>=0.4.6",
+        "colorclass>=2.2.2",
+        "cryptography>=43.0.0",
+        "easygui>=0.98.3",
+        "filetype>=1.2.0",
+        "idna>=3.7",
+        "lxml>=5.3.0",
+        "msoffcrypto-tool>=5.4.2",
+        "olefile>=0.47",
+        "oletools>=0.60.2",
+        "pcodedmp>=1.2.6",
+        "pefile>=2023.2.7",
+        "pycparser>=2.22",
+        "pyfiglet>=1.0.2",
+        "pyparsing>=3.1.2",
+        "PyPDF2>=3.0.1",
+        "python-docx>=1.1.2",
+        "python-magic>=0.4.27",
+        "requests>=2.32.3",
+        "setuptools>=73.0.1",
+        "tabulate>=0.9.0",
+        "typing_extensions>=4.12.2",
+        "urllib3>=2.2.2",
+        "win_unicode_console>=0.5"
+    ],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
@@ -50,4 +51,9 @@ setup(
         'Development Status :: 3 - Alpha'
     ],
     python_requires='>=3.11',
+    entry_points={
+        'console_scripts': [
+            'Pyth3rnalisis=Pyth3rNalisis:main',
+        ],
+    },
 )
---
+++ src/__main__.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+
+def main():
+    #imports
+    import sys
+    import os
+    import argparse
+    import modules.module_log as module_log
+    import modules.module_banner as module_banner
+    import modules.worker_metadata as worker_metadata
+    import modules.worker_extension as worker_extension
+    import modules.worker_hashingAnalisis as worker_hashingAnalisis
+    import modules.worker_entropy as worker_entropy
+
+    #constants
+    version = '0.0.4'
+
+    # Custom ArgumentParser class
+    class CustomArgumentParser(argparse.ArgumentParser):
+        def print_help(self, file=None):
+            module_banner.printBanner(version)  # Print the banner
+            super().print_help(file)  # Call the default print_help()
+
+    # Argument parsing with custom parser
+    parser = CustomArgumentParser(
+        prog='Pyth3rNalisis',
+        description='Pyth3rNalisis is a malware analysis tool that searches for red flags in any file. [NOT A REPLACEMENT FOR AV!!!!]'
+    )
+    parser.add_argument("-f", "--file", help="File to check")
+    parser.add_argument("-e", "--extension", action='store_true', help="Check for extension anomaly")
+    parser.add_argument("-E", "--entropy", action='store_true', help="Check for unusal entropy in file")
+    parser.add_argument("-H", "--hashing", action='store_true', help="Perform hashing analisis on the file")
+    parser.add_argument("-m", "--metadata", action='store_true', help="Check for metadata anomaly")
+    args = parser.parse_args()
+
+    module_banner.printBanner(version)  # Print the banner
+
+    # Check if a file path argument is provided
+    if not args.file:
+        module_log.critical('No file specified, quitting')
+        sys.exit(1) # Exit the program with a non-zero status to indicate an error
+    elif not os.path.isfile(args.file):
+        module_log.critical(f"File '{args.file}' specified does not exist, quitting")
+        sys.exit(1) # Exit the program with a non-zero status to indicate an error
+    else:
+        # Recursively check if any flags are specified
+        flags_provided = any(value for key, value in vars(args).items() if key != 'file')
+        
+        if not flags_provided:
+            module_log.critical('No analysis flags specified, quitting')
+            sys.exit(1)  # Exit the program with a non-zero status to indicate an error
+
+    # Handle extension analysis
+    if args.extension:
+        print('\033[91m==================\033[0m')
+        print('\033[92mEXTENSION ANALISIS:\033[0m')
+        worker_extension.check_file_type(args.file)
+
+    # Handle hashing analysis
+    if args.hashing:
+        print('\033[91m==================\033[0m')
+        print('\033[92mHASHING ANALISIS:\033[0m')
+        worker_hashingAnalisis.perform_hashing_analisis(args.file)
+
+    # Handle metadata analysis
+    if args.metadata:
+        print('\033[91m==================\033[0m')
+        print('\033[92mMETADATA ANALISIS:\033[0m')
+        worker_metadata.check_metadata(args.file)
+
+    #Handle entropy analysis
+    if args.entropy:
+        print('\033[91m==================\033[0m')
+        print('\033[92mENTROPY ANALISIS:\033[0m')
+        worker_entropy.check_entropy(args.file)
+
+if __name__ == "__main__":
+    main()
